fe6b243c060e617c7d3094c6058a41a84c702f63
andress722 <andrecidre@hotmail.com>
2022-07-13 17:18:00 -0300

    make it better

--- controller/pedidoController.js
+++ controller/pedidoController.js
@@ -29,10 +29,12 @@ const pedidoController = {
 
       adicionaPost: async(req,res)=> {
         const idProdutos = req.params.idProdutos
+        const usuario = req.session.usuarioLogado.id
         console.log(idProdutos)
         try {
             await Carrinho.create({
-                id_produto: idProdutos
+                id_produto: idProdutos,
+                id_usuario: usuario
             })
             
             return res.redirect('/pedidos/vercarrinho')
@@ -74,6 +76,7 @@ const pedidoController = {
       
        enviarPedido: async function(req,res){
         
+        const usuario = req.session.usuarioLogado.id
       
         const produtos = req.body
 
@@ -85,7 +88,8 @@ const pedidoController = {
               produto: produtos[i].produto,
               quantidade: produtos[i].quantidade,
               valor: produtos[i].valor,
-              id_produto: produtos[i].id
+              id_produto: produtos[i].id,
+              id_usuario: usuario
             })
             // array.push(produtos[i].produto)
             // array.push(produtos[i].quantidade)
